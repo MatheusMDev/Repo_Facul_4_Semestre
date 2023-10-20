9aa99955a37b8845c701016fbd377a38ef0dd285
Matheus <matheu.miranda@hotmail.com>
2023-10-20 17:27:12 -0300

    Send New Project

---
+++ Linguagem de Programacao I/Ecommerce/Ecommerce/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models
+{
+    public class Categoria
+    {
+        // atributos = propriedades
+        public int IdCategoria { get; set; }
+        public string NomeCategoria { get; set; }
+    }
+}
---
+++ Linguagem de Programacao I/Ecommerce/Ecommerce/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models
+{
+    public class Produto
+    {
+        // atributos = propriedades
+        public int idProduto { get; set; }
+        public string NomeProd { get; set; }
+        public string Descricao { get; set; }
+        public double Valor { get; set; }
+        public string urlImagem { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
